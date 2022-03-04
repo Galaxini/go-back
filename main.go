6b96387eca1dcb6ea72298d3152f6175fbc2efb2
Galaxini <skotchslowhand@gmail.com>
2022-03-04 12:29:09 +0900

    upd

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"log"
+	"my-app-back-go/controllers"
+	"my-app-back-go/database"
+	"my-app-back-go/entity"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql" //Required for MySQL dialect
+)
+
+func main() {
+	initDB()
+	log.Println("Starting the HTTP server on port 8090")
+
+	router := mux.NewRouter().StrictSlash(true)
+	initaliseHandlers(router)
+	log.Fatal(http.ListenAndServe(":8090", router))
+}
+
+func initaliseHandlers(router *mux.Router) {
+	router.HandleFunc("/create", controllers.CreatePerson).Methods("POST")
+	router.HandleFunc("/get", controllers.GetAllPerson).Methods("GET")
+	router.HandleFunc("/get/{id}", controllers.GetPersonByID).Methods("GET")
+	router.HandleFunc("/update/{id}", controllers.UpdatePersonByID).Methods("PUT")
+	router.HandleFunc("/delete/{id}", controllers.DeletPersonByID).Methods("DELETE")
+}
+
+func initDB() {
+	config :=
+		database.Config{
+			ServerName: "127.0.0.1:3306",
+			User:       "root",
+			Password:   "",
+			DB:         "my_app_db",
+		}
+
+	connectionString := database.GetConnectionString(config)
+	err := database.Connect(connectionString)
+	if err != nil {
+		panic(err.Error())
+	}
+	database.Migrate(&entity.Person{})
+}
